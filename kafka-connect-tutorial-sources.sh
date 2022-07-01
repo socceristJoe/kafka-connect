957318537a7f6a327cab1d12e5f952dd9b1b0025
Qiao, Joe <qiao.y@pg.com>
2022-07-01 11:44:28 +0800

    first run

--- kafka-connect-tutorial-sources.sh
+++ kafka-connect-tutorial-sources.sh
@@ -5,6 +5,7 @@
 
 # 1) Source connectors
 # Start our kafka cluster
+cd /Users/joeqiao/Documents/LocalHub/kafka/kafka-connect/
 docker-compose up kafka-cluster
 # Wait 2 minutes for the kafka cluster to be started
 
